bf358e1352614f528f4a71f84dad245d3a05c422
sagun bajpai <sagunbajpaisagun@gmail.com>
2026-01-17 19:55:37 +0530

    new add file

---
+++ Sorting_with_java37/mergeSort.java
@@ -0,0 +1,57 @@
+package Sorting_with_java37;
+
+public class mergeSort {
+    static void merge(int[] arr, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        int i, j, k;
+        for (i = 0; i < n1; i++)
+            left[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            right[j] = arr[mid + 1 + j];
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            arr[k++] = right[j++];
+        }
+        while (i < n1)
+            arr[k++] = left[i++];
+        while (j < n2)
+            arr[k++] = right[j++];
+
+    }
+
+    static void MergeSort(int[] arr, int left, int right) {
+        if (left == right)
+            return;
+        int mid = (left + right) / 2;
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    static void display(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 3, 6, 2, 7, 1 };
+        int n = arr.length;
+        System.out.println("Array before sorting");
+        display(arr);
+        System.out.println();// using next line
+        System.out.println("Array after sorting ");
+        MergeSort(arr, 0, n - 1);
+        display(arr);
+
+    }
+
+}
