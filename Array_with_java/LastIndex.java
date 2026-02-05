8388ce788cfafe103ee4838eac9718efc46a6d34
sagun bajpai <sagunbajpaisagun@gmail.com>
2026-02-05 12:34:13 +0530

    some change

--- Array_with_java/LastIndex.java
+++ Array_with_java/LastIndex.java
@@ -1,28 +1,33 @@
 //find the last occurrences of an element x in a given array
+/*“Main poora array left se right dekhunga,
+jab-jab x milega, index update karta jaunga.
+End me jo index bachega, wahi last occurrence hoga.” */
 package Array_with_java;
+
 import java.util.Scanner;
+
 public class LastIndex {
-    static int LastOccurrences(int[] arr,int x){
-        int LastIndex=-1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==x){
-                LastIndex=i;
+    static int LastOccurrences(int[] arr, int x) {
+        int LastIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                LastIndex = i;
             }
         }
         return LastIndex;
- }
-        public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of array:");
-        int n=sc.nextInt();
-        int []arr=new int[n];
-        System.out.println("enter the element:"+n);
-        for(int i=0;i<arr.length;i++){
-            arr[i]=sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the element:" + n);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
         System.out.println("enter the value of x:");
-        int x=sc.nextInt();
-        System.out.println("last occurrences of x: "+ LastOccurrences(arr,x));
+        int x = sc.nextInt();
+        System.out.println("last occurrences of x: " + LastOccurrences(arr, x));
     }
 }
- 
